f01f53a65fd4ea5d8e78f919fc8fc8c7de81528a
cathydk <113735951+cathydk@users.noreply.github.com>
2026-03-17 12:24:51 -0700

    Add files via upload

---
+++ Exercises/odd_even.py
@@ -0,0 +1,36 @@
+'''1. Even or Odd Checker
+
+Concepts: conditionals, user input
+Write a program that:
+
+Takes a number as input
+
+Prints whether it’s even or odd
+
+👉 Challenge: Handle invalid input (like letters instead of numbers)'''
+
+if __name__ == '__main__':
+    
+    # variable to exit loop
+    # variable to check if input is an integer
+    is_valid_input = False
+    
+    # will keep asking for user input until a valid input is given
+    while is_valid_input == False:
+        # asking for user input
+        # input will be read as a string
+        user_input_string = input('Enter a number: ')
+        
+        # handling invalid input
+        try:
+            # try to convert string to int if possible
+            user_input_int = int(user_input_string)
+            # checking to see if user input is odd or even
+            if user_input_int%2 == 0:
+                print('You entered an even number!')
+            else:
+                print('You entered an odd number!')
+            is_valid_input = True
+        # user did not input an integer value
+        except ValueError:
+            print('You did not enter a number, please enter a number: ')
