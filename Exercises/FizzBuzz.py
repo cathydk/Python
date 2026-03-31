e7e83814c0267b49b33bf38aef5d6d47bb1f0d6f
cathydk <113735951+cathydk@users.noreply.github.com>
2026-03-31 13:50:16 -0700

    Create FizzBuzz.py

---
+++ Exercises/FizzBuzz.py
@@ -0,0 +1,36 @@
+'''2. FizzBuzz
+
+Concepts: loops, conditionals (classic interview question)
+
+Print numbers from 1 to 50:
+
+If divisible by 3 → print "Fizz"
+If divisible by 5 → print "Buzz"
+If divisible by both → print "FizzBuzz"
+
+👉 Challenge: Let the user choose the range'''
+
+if __name__ == '__main__':
+
+    # let user know what the program does
+    print('FizzBuzz')
+    print('If divisible by 3 → print "Fizz"')
+    print('If divisible by 5 → print "Buzz"')
+    print('If divisible by both → print "FizzBuzz"')
+
+    # letting the user choose the range
+    # convert user input from string to int
+    min_range = int(input('Pick the number for the starting range: '))
+    max_range = int(input('Pick the number for the ending range: '))
+
+    # loop through the range the user inputted
+    # start = min_range
+    # stop = max_range
+    for i in range(min_range, max_range):
+        print(i)
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
