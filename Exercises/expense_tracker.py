7a6fbba24f13d9c26390bbdc240cf102e12e57a3
cathydk <113735951+cathydk@users.noreply.github.com>
2026-04-09 16:40:50 -0700

    Create expense_tracker.py

---
+++ Exercises/expense_tracker.py
@@ -0,0 +1,22 @@
+'''5. Expense Tracker (Dictionary) ⭐
+
+Concepts: dictionaries, lists
+
+Build a program that:
+
+Lets users enter expenses like:
+Food: 20
+Transport: 10
+Stores them in a dictionary
+Calculates:
+Total spending
+Spending by category
+
+👉 Example:
+
+{"Food": [20, 15], "Transport": [10]}
+
+👉 Challenge:
+
+Show which category has the highest spending
+Allow deleting an expense'''
