85ddf16b2dc3107730a42013a1b270c07f25686f
cathydk <113735951+cathydk@users.noreply.github.com>
2026-04-10 15:44:46 -0700

    Update expense_tracker.py

--- Exercises/expense_tracker.py
+++ Exercises/expense_tracker.py
@@ -25,29 +25,45 @@ if __name__ == '__main__':
 
     # dictionary to hold category and how much spent
     tracker = {}
-    # list (value) to hold how much user spent
+    # list (value) to hold how much user spent************************
     expense = []
 
     # let user know what program does
     print('Expense Tracker')
+    print('---------------')
+    print('1. Add an expense')
+    print('2. Calculate total spending')
+    print('3. Show spending by category')
+    print('4. Show which category has the highest spending')
+    print('5. Delete an expense')
+    print('6. Quit')
 
     # allow users to keep using tracker until they decide to quit
     while True:
-        # handles when user wants to quit
-        quit = input('Type in \'q\' when you want to quit the program: ')
-        if quit == 'q':
-            break
-        # get category from user
+
+        # get what user chooses in the option menu
+        user_choice = input('Choose an option: ')
+
+        '''# get category from user
         category = input('Enter in a category: ')
+        # get amount spent from user************************
+        amount_spent = int(input('Enter in how much you spent: '))
+        expense.append(amount_spent)
+        # add the user input to the dictionary; category = key, expense = value************************
+        tracker[category] = expense
+
         # go through dictionary and only add categories that haven't been added
-        for key, value in tracker.items():
+        for key, values in tracker.items():
+            # get category from user
+            category = input('Enter in a category: ')
             if key in tracker:
                 add_amount = int(input('This category already exist, enter in how much you spent: '))
                 expense.append(add_amount)
                 tracker[category] = expense
-        # get amount spent from user
-        amount_spent = int(input('Enter in how much you spent: '))
-        expense.append(amount_spent)
-        # add the user input to the dictionary; category = key, amount_spent = value
-        tracker[category] = expense
-        print(tracker)
+            else:
+                # get amount spent from user
+                amount_spent = int(input('Enter in how much you spent: '))
+                expense.append(amount_spent)
+                # add the user input to the dictionary; category = key, amount_spent = value
+                tracker[category] = expense
+        print(tracker)'''
