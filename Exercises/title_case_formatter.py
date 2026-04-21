1b16bc7dd0929acee6ebdcac7c88f369a700e022
cathydk <113735951+cathydk@users.noreply.github.com>
2026-04-21 13:49:32 -0700

    Update title_case_formatter.py

--- Exercises/title_case_formatter.py
+++ Exercises/title_case_formatter.py
@@ -62,18 +62,82 @@ Output: "The Lord of the Rings"
 ```
 '''
 
+# define function to format string title
+# sentence is a string
+def to_title_case(sentence):
+    # to hold formatted title
+    formatted_sentence = ''
+    # list to hold words that don't need to be capitalized if it's in the middle of the title
+    no_capitalize = ['and', 'the', 'of', 'in']
+    # use .split() to turn sentence (string) into a list
+    sentence_list = sentence.split()
+
+    # first word will always be capitalized, even if it is 'and', 'the', 'of', or 'in'
+    # sentence_list[0:1] handles only the first word in the list*********************************
+    for w in sentence_list[0:1]:
+        # capitalize the first letter in the word
+        formatted_sentence += w[0].upper()
+        # make the trailing characters lowercase
+        formatted_sentence += w[1:].lower()
+        # adds a space in between word
+        formatted_sentence += ' '
+
+    # loop through sentence, starting at the second element in the list
+    for word in sentence_list[1:]:
+        # checks if current word is in no_capitalize list or not
+        if word not in no_capitalize:
+            # if current word is not in no_capitalize list
+            # then capitalize the first letter of the word
+            # make the trailing characters lowerecase
+            # store it in formatted_sentence
+            formatted_sentence += word[0].upper()
+            formatted_sentence += word[1:].lower()
+            # adds a space in between each word
+            formatted_sentence += ' '
+        else:
+            # if current word is in no_capitalize list
+            # then don't capitalize the first letter of the word
+            # store it in formatted_sentence
+            formatted_sentence += word
+            # adds a space in between each word
+            formatted_sentence += ' '
+
+    # return formatted title (string)
+    return formatted_sentence
+
+
+# define function to get user input, call function, and display output
+def main():
+    user_sentence = input('Enter in a sentence: ')
+    # user_sentence (string) is being passed in
+    # formatted_title (string) holds the return value of to_title_case(sentence) function
+    formatted_title = to_title_case(user_sentence)
+    print(formatted_title)
+
+# calls main() function
+if __name__ == '__main__':
+    main()
+
+
+'''
 def to_title_case(sentence):
     formatted_sentence = ''
     no_capitalize = ['and', 'the', 'of', 'in']
     sentence_list = sentence.split()
 
-    for word in sentence_list:
+    for w in sentence_list[0:1]:
+        formatted_sentence += w[0].upper()
+        formatted_sentence += w[1:].lower()
+        formatted_sentence += ' '
+
+    for word in sentence_list[1:]:
         if word not in no_capitalize:
             formatted_sentence += word[0].upper()
             formatted_sentence += word[1:].lower()
             formatted_sentence += ' '
         else:
             formatted_sentence += word
+            formatted_sentence += ' '
 
     return formatted_sentence
 
@@ -84,3 +148,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    '''
